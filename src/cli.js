df258a64d9fd241f0f38b6ab28924e759b16b664
Larisa <isaykinalarisa@gmail.com>
2022-05-18 21:35:29 +0600

    add .eslint.cjs

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,7 @@
 import readlineSync from "readline-sync";
+
 export const greeting = () => {
-    const userName = readlineSync.question("May I have your name?");
-    const result = console.log(`Hello, ${userName}!`);
-    return result;
+  const userName = readlineSync.question("May I have your name?");
+  const result = console.log(`Hello, ${userName}!`);
+  return result;
 };
