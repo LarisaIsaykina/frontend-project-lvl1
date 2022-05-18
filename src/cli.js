a22a11877d628e8868fbbf34624b11339abf64d8
Larisa <isaykinalarisa@gmail.com>
2022-05-18 14:39:49 +0600

    add brain-games.js cli.js

---
+++ src/cli.js
@@ -0,0 +1,6 @@
+import readlineSync from "readline-sync";
+export const greeting = () => {
+    const userName = readlineSync.question("May I have your name?");
+    const result = console.log(`Hello, ${userName}!`);
+    return result;
+};
