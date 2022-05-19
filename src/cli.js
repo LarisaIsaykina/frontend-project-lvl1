5d6df12373c5163749f47e824287dd35a2114973
Larisa <isaykinalarisa@gmail.com>
2022-05-19 12:24:39 +0600

    modified cli.js brain-games.js

--- src/cli.js
+++ src/cli.js
@@ -1,5 +1,6 @@
 import readlineSync from 'readline-sync';
 
+// eslint-disable-next-line import/prefer-default-export
 export const greeting = () => {
   const userName = readlineSync.question('May I have your name?');
   const result = console.log(`Hello, ${userName}!`);
